11830ad669b525d73435c2febeb49b0af71fd2f2
Vedant Agarwal <vedag812@gmail.com>
2026-02-18 10:32:10 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -13,15 +13,16 @@ public class PalindromeCheckerApp {
     }
 
     static void PalindromeChecker(String str) {
-        String rev = "";
+        int n = str.length();
         for(int i = str.length()-1; i >= 0; i--) {
-            rev += str.charAt(i);
-        }
 
-        if(str.equals(rev)) {
-            System.out.println(str + " is Palindrome.");
-        } else {
-            System.out.println(str + " is not Palindrome.");
+            if (str.charAt(i) != str.charAt(n - i -1)) {
+                System.out.println(str + " is not Palindrome.");
+                break;
+            } else {
+                System.out.println(str + " is Palindrome.");
+                break;
+            }
         }
     }
 }

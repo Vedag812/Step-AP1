cf2356006039cbc8c61fe3c164426eaf14e26191
Vedant Agarwal <vedag812@gmail.com>
2026-02-18 09:57:41 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,18 +3,5 @@ import java.util.Arrays;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome Palindrome");
-        int n = 1001;
-        PalindromeChecker(n);
-    }
-
-    static void PalindromeChecker (int number) {
-        String orig = String.valueOf(number);
-        String rev = new StringBuilder(orig).reverse().toString();
-
-        if(orig.equals(rev)) {
-            System.out.println(number + " is a Palindrome number.");
-        } else {
-            System.out.println(number + " is not a Palindrome number.");
-        }
     }
 }

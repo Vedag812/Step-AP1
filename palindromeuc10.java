6a8884f25418f8092e534063ac5b7fe99546d4df
Vedant Agarwal <vedag812@gmail.com>
2026-02-25 09:05:59 +0530

    Add files via upload

---
+++ palindromeuc10.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class palindromeuc10
+{
+    public static void main(String[] args)
+    {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter input:");
+        String input = sc.nextLine();
+
+
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        boolean isPalindrome = true;
+        for (int i = 0; i < normalized.length() / 2; i++) {
+
+            if (normalized.charAt(i) !=
+                    normalized.charAt(normalized.length() - 1 - i)) {
+
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Is Palindrome?: " + isPalindrome);
+
+        sc.close();
+    }
+}
